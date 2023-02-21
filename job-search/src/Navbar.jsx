3d5bda5378b49d803a3d368622c33b239f1e73f9
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2023-02-21 06:16:10 -0800

    added Adzuna logo to navbar

--- job-search/src/Navbar.jsx
+++ job-search/src/Navbar.jsx
@@ -1,19 +1,22 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
+import { Icon } from '@iconify/react';
+import adzunaIcon from './adzuna.svg';
 
 function Navbar() {
   return (
     <header>
       <nav className='top-nav'>
-        <ul className="links-container">
-          <li>
-            <Link to='/'>Home</Link>
+        <ul className='links-container'>
+          <li id='home-button'>
+            <Link to='/'>
+              <Icon icon='material-symbols:home' id='home-icon' />
+            </Link>
           </li>
         </ul>
-        <div className="icon-container">
-          
-        </div>
+        <h2 id='nav-title'>Job Search via Adzuna</h2><img src={adzunaIcon} alt='adzuna logo' id='adzuna-icon'/>
       </nav>
     </header>
   )
 }
+
 export default Navbar
